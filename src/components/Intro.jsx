7e51bbbbbcd9fe366f8b0f109e9cc1f515005036
Zeri Aaron <zeriaaron23@gmail.com>
2025-08-10 22:27:59 +0800

    Created a link to the booking page using the button

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import introStyle from '../styles/Intro.module.css'
 import restaurantFood from '../assets/images/restaurantfood.jpg'
+import { Link } from 'react-router-dom'
 
 const Intro = () => {
     return (
@@ -10,7 +11,7 @@ const Intro = () => {
                     <h2>Little Lemon</h2>
                     <h3>Chicago</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit repellendus natus iure maiores omnis repudiandae aliquid sit obcaecati fugiat numquam, assumenda, itaque similique delectus minima.</p>
-                    <button type="submit">Reserve a Table</button>
+                    <button type="button"><Link to={'/book'}>Reserve a Table</Link></button>
                 </article>
                 <img src={restaurantFood} alt="Restaurant Food" />
             </section>
