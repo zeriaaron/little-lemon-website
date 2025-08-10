d367debae81deeb46e6a0092b1327b555995c5d4
Zeri Aaron <zeriaaron23@gmail.com>
2025-08-10 16:12:11 +0800

    Configured routing and changed the structure of Nav and App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css'
 import Header from './components/Header'
-import Main from './components/Main'
+import Home from './pages/Home'
 import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Header>
-      </Header>
-      <Main/>
+      <Header/>
+
+      {/* Routing */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+
       <Footer/>
     </>
+    // Updating the navigation to match the req of Single Page Applications principle
+    // npm i react-router-dom
+    // import Browser Router from react-router-dom [main.jsx]
+    // import Routes, Route from react-router-dom [here]
+    // use Link instead of li [Nav.jsx]
+    // Learned: Put Routes in betwen the Header and Footer to do "like" a conditional rendering components
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,34 @@
 import logo from '../assets/images/Logo.svg'
 import navStyle from '../styles/Nav.module.css'
+import classNames from 'classnames'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
+        // <nav>
+        //     <ul className={`${navStyle.unorderedList} ul`}>
+        //         <img className={`${navStyle.img}`} src={logo} alt="Little Lemon Logo" />
+        //         <li><a className={`${navStyle.links}`} href="">Home</a></li>
+        //         <li><a className={`${navStyle.links}`} href="">About</a></li>
+        //         <li><a className={`${navStyle.links}`} href="">Menu</a></li>
+        //         <li><a className={`${navStyle.links}`} href="">Reservations</a></li>
+        //         <li><a className={`${navStyle.links}`} href="">Order Online</a></li>
+        //         <li><a className={`${navStyle.links}`} href="">Login</a></li>
+        //     </ul>
+        // </nav>
+
+        // Updating the navigation
+        // Learned: ul, li are semantics and should be partnered always
+        // Learned: Link has "a" anchor tag that you can target to work with styling
         <nav>
-            <ul className={`${navStyle.unorderedList} ul`}>
+            <ul className={classNames(navStyle.unorderedList)}>
                 <img className={`${navStyle.img}`} src={logo} alt="Little Lemon Logo" />
-                <li><a className={`${navStyle.links}`} href="">Home</a></li>
-                <li><a className={`${navStyle.links}`} href="">About</a></li>
-                <li><a className={`${navStyle.links}`} href="">Menu</a></li>
-                <li><a className={`${navStyle.links}`} href="">Reservations</a></li>
-                <li><a className={`${navStyle.links}`} href="">Order Online</a></li>
-                <li><a className={`${navStyle.links}`} href="">Login</a></li>
+                <li><Link className={`${navStyle.links}`} to={'/'}>Home</Link></li>
+                <li><Link className={classNames(navStyle.links)} to={'/'}>About</Link></li>
+                <li><Link className={classNames(navStyle.links)} to={'/'}>Menu</Link></li>
+                <li><Link className={classNames(navStyle.links)} to={'/'}>Reservations</Link></li>
+                <li><Link className={classNames(navStyle.links)} to={'/'}>Order Online</Link></li>
+                <li><Link className={classNames(navStyle.links)} to={'/'}>Login</Link></li>
             </ul>
         </nav>
     )
